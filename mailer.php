93fe35069bcf9ca9eae0c104ff5491f804bcfff0
oiver55 <osaintilien55@gmail.com>
2024-06-09 04:20:53 -0400

    updated contact information and added email to mailer.php

--- mailer.php
+++ mailer.php
@@ -20,7 +20,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "support@reactheme.com";
+        $recipient = "osaintDev@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
